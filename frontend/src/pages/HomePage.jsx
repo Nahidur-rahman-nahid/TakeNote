7e02f041dc9a2b7b6a1677a2aa0bd94aa38308ec
Nahidur-rahman-nahid <119045215+Nahidur-rahman-nahid@users.noreply.github.com>
2025-06-19 13:14:14 +0600

    Uploaded the full project

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,9 +2,10 @@ import React,{useState} from 'react'
 import Navbar from  "../components/Navbar.jsx"
 import RateLimitedUI from '../components/RateLimitedUI.jsx';
 import { useEffect } from 'react';
-import axios from 'axios';
 import toast from 'react-hot-toast';
-
+import NoteCard from '../components/NoteCard.jsx';
+import api from "../lib/axios.js";
+import NoteNotFound from '../components/NoteNotFound.jsx';
 
 
 const HomePage = () => {
@@ -15,7 +16,7 @@ const HomePage = () => {
 useEffect( () => {
         const fetchNotes = async () => {
              try {
-                const res = await axios.get("http://localhost:5001/api/notes");
+                const res = await api.get("/notes");
                 const data = res.data;
                 console.log(data)
                 setNotes(res.data)
@@ -41,6 +42,24 @@ useEffect( () => {
     <div className='min-h-screen'>
       <Navbar/>
       {israteLimited && <RateLimitedUI/>}
+       <div className='max-w-7xl mx-auto p-4 mt-6'>
+         {loading && <div className='text-center text-primary py-10'>Loading Notes...</div>}
+         {notes.length === 0 && !israteLimited && <NoteNotFound/>}
+         {notes.length > 0 && !israteLimited && (
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+               {notes.map((note) => (
+                  // <div>
+                  //     {note.title} | {note.content}
+                  // </div>
+                  <NoteCard key={note._id} note={note} setNotes={setNotes} />
+
+               ))}
+
+
+            </div>
+         )}
+       </div>
+
     </div>
   )
 }
