49f79ebe0f095b85b5f7995ed95132dba2097888
Nahidur-rahman-nahid <119045215+Nahidur-rahman-nahid@users.noreply.github.com>
2025-06-20 00:53:10 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,28 +3,30 @@ import { connectDB } from "./config/db.js";
 import notesRoute from "./routes/notesRoute.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
-const app = express()
+import dotenv from "dotenv";
 
+// Load env variables
+dotenv.config();
 
+const app = express();
 
-//middleware
-app.use(express.json())
+// middleware
+app.use(express.json());
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true, // optional: if you're using cookies or auth headers
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
   })
 );
-app.use(rateLimiter)
-app.use("/api/notes",notesRoute)
 
+app.use(rateLimiter);
+app.use("/api/notes", notesRoute);
 
-connectDB().then( () => {
-    app.listen(5001, () => {
-    console.log("server started on PORT: 5001");
+// Connect DB and start server
+connectDB().then(() => {
+  const PORT = process.env.PORT || 5001;
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+  });
 });
-}
-
-);
-
-//
