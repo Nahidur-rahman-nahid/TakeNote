d62102b30d97c44feab3cba3343427280651dcae
Nahidur-rahman-nahid <119045215+Nahidur-rahman-nahid@users.noreply.github.com>
2025-06-21 17:00:20 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URI || "http://localhost:5173",
     credentials: true,
   })
 );
